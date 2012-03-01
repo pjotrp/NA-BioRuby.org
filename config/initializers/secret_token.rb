25b8167549473877a0aca1d6a64995a65ea9b057
Marjan Povolni <marian.povolny@gmail.com>
2012-03-01 11:30:09 +0100

    Created a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NABiorubyOrg::Application.config.secret_token = '5a333f42de3a324f8c9eee5c43577ac64eabbd2437d9ab67582b5276fee30eeeb1e72bb145b63c54e3050b8b240bcfe152a7158ba545262c452dc14c709477cb'
